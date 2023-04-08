571b5e400d06d07381d5cfcd7fdaaa42c47fe731
aijkl <aijkl@outlook.jp>
2023-04-08 21:56:05 +0900

    プロジェクト ファイルを追加します。

---
+++ OpenWeight/Commands/DaemonCommand.cs
@@ -0,0 +1,12 @@
+﻿using Spectre.Console.Cli;
+
+namespace OpenWeight.Commands
+{
+    internal class DaemonCommand : AsyncCommand
+    {
+        public override Task<int> ExecuteAsync(CommandContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
