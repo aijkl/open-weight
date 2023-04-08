ec8c6d900f2e081bb6362ad0860f1cc165555421
aijkl <aijkl@outlook.jp>
2023-04-09 00:20:30 +0900

    CLIの説明を追加

--- OpenWeight.Cli/Program.cs
+++ OpenWeight.Cli/Program.cs
@@ -10,7 +10,7 @@ internal class Program
         var commandApp = new CommandApp();
         commandApp.Configure(x =>
         {
-            x.AddCommand<DaemonCommand>("daemon");
+            x.AddCommand<DaemonCommand>("daemon").WithDescription("常時通信をして体重を表示します").WithExample(new[] { "AA:89:5E:18:DA:91", "hci0", "--interval 300" });
         });
         return await commandApp.RunAsync(args);
     }
